5666a87a1db47cfb875e4bd6947dc432c245c9e8
shifraDomb <do8259@gmail.com>
2023-05-24 15:47:12 +0300

    checkeslint

--- Tests/TestFunction.test.js
+++ Tests/TestFunction.test.js
@@ -1,16 +1,17 @@
 const validations=require('./functions');
-const cors=require('../controllers/controler')
-const app = require('../app');
 
 test('check if the emailvalid',()=>{
-    expect(validations.validateEmail("sss@gmail.com")).toBe(true);
-}) 
+    expect(validations.validateEmail('sss@gmail.com')).toBe(true);
+});
+ 
 test('check if the email contain @',()=>{
-    expect(validations.validateEmail("sssgmail.com")).toBe(false);
-})
+    expect(validations.validateEmail('sssgmail.com')).toBe(false);
+});
+
 test('check if the name is correct',()=>{
-    expect(validations.validateName("kkk")).toBe(true);
-})
+    expect(validations.validateName('kkk')).toBe(true);
+});
+
 test('check if the name is correct',()=>{
-    expect(validations.validateName("88")).toBe(false);
-})
+    expect(validations.validateName('88')).toBe(false);
+});
