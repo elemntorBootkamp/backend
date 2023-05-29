55e843ebd7c971c68c7b9791e23989133468b8b0
shifraDomb <do8259@gmail.com>
2023-05-29 09:28:18 +0300

    githooks

--- Tests/TestFunction.test.js
+++ Tests/TestFunction.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const validations=require('./functions');
 
 test('check if the emailvalid',()=>{
@@ -9,7 +10,7 @@ test('check if the email contain @',()=>{
 });
 
 test('check if the name is correct',()=>{
-    expect(validations.validateName("kkk")).toBe(true);
+    expect(validations.validateName('kkk')).toBe(true);
 });
 
 test('check if the name is correct',()=>{
