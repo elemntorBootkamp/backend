60d651a148adba0943525f48ccd9ffef8b9036bb
RivkyBaum <rivky3254@gmail.com>
2023-06-01 12:37:57 +0300

    jjj

--- Tests/TestFunction.test.js
+++ Tests/TestFunction.test.js
@@ -1,7 +1,9 @@
 const validations = require('./functions');
-//const cors=require('../controllers/controler');
+const request = require('supertest');
+const cors=require('../controllers/controler');
 const app = require('../app');
 const axios = require('axios');
+const listUsers = require('../controllers/controler')
 
 test('check if the emailvalid', () => {
     expect(validations.validateEmail('sss@gmail.com')).toBe(true);
@@ -15,3 +17,10 @@ test('check if the name is correct', () => {
 test('check if the name is correct', () => {
     expect(validations.validateName('88')).toBe(false);
 });
+describe('GET /api/getAll', () => {
+    it('responds with a list of users', async () => {
+      const response = await request(app.js).get('/api/getAll');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(listUsers);
+    });
+  });
