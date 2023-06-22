12dfb6c31185ea7be3a447438c3ab60f597968cc
shifraDomb <do8259@gmail.com>
2023-06-22 09:23:03 +0300

    a

--- Routes/websiteRout.js
+++ Routes/websiteRout.js
@@ -1,8 +1,6 @@
-const express=require('express')
-const websiterout=express.Router()
-const {
-    update
-} = require('../controllers/websitController');
+const express = require('express');
+const websiterout = express.Router();
+const { update } = require('../controllers/websitController');
 
-websiterout.put('/update', update)
-module.exports= websiterout
+websiterout.put('/update', update);
+module.exports = websiterout;
